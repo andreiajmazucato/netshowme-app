e0390c56faa552fa86a7c1744d44e9dc6c85e582
Andreia <andreiajmazucato@gmail.com>
2025-04-15 12:12:21 -0300

    Ajuste

--- src/tests/VideoCardTest.ts
+++
@@ -1,8 +0,0 @@
-// tests/VideoCard.test.tsx
-import { render, screen } from '@testing-library/react'
-import VideoCard from '../components/VideoCard'
-
-test('renders video title', () => {
-    render(<VideoCard video={{ id: 1, title: 'Teste', thumbnail: 'test.jpg' }} />)
-    expect(screen.getByText('Teste')).toBeInTheDocument()
-})
